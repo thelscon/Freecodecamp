2ea8cdbd5c8c7ef4695689a32e0f61dd6ab2ae7d
thelscon <thelscon@gmail.com>
2022-01-31 22:59:48 +0200

    add Find_the_Longest_Word_in_a_String.js

---
+++ Find_the_Longest_Word_in_a_String.js
@@ -0,0 +1,28 @@
+//              Найдите самое длинное слово в строке
+// Возвращает длину самого длинного слова в предоставленном предложении. Ваш ответ должен быть числом.
+
+// первый способ
+function findLongestWordLength(str) {
+    let tmp = str.split ( ' ' ) ;
+    let maxLength = 0 ;
+    tmp.forEach ( value => 
+        maxLength = value.length > maxLength ? value.length : maxLength ) ;
+    return maxLength;
+}
+  
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+/* // второй способ
+function findLongestWordLength(str) {
+    let tmp = str.split ( ' ' ) ;
+    let maxLength = 0 ;
+    tmp.forEach ( value => {
+        if ( value.length > maxLength ) {
+            maxLength = value.length ;
+            str = value ;
+        }
+    } ) ;
+    return str.length;
+}
+  
+findLongestWordLength("The quick brown fox jumped over the lazy dog"); */
