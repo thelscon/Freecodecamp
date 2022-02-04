f5d562291671d3217aaaf16a961dce96b7c36d6e
thelscon <thelscon@gmail.com>
2022-02-04 14:43:23 +0200

    Truncate_a_String.js

---
+++ Truncate_a_String.js
@@ -0,0 +1,12 @@
+//             Обрезать строку
+// Обрезать строку (первый аргумент), если она длиннее заданной максимальной длины строки (второй аргумент). Вернуть усеченную 
+// строку с ...окончанием.
+
+function truncateString(str, num) {
+    if ( str.length > num ) {
+        str = str.substr ( 0, num ) + '...' ;
+    }
+    return str;
+}
+  
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
