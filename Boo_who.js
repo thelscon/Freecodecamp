eee3cc62dca68569ca36fe268916ba04f53f4ad1
thelscon <thelscon@gmail.com>
2022-02-04 19:27:05 +0200

    Boo_who.js

---
+++ Boo_who.js
@@ -0,0 +1,11 @@
+/*             Бу, кто
+Проверьте, классифицируется ли значение как логический примитив. Возврат trueили false.
+Булевы примитивы trueи false. */
+
+function booWho(bool) {
+
+    return  ( typeof bool === 'boolean' ) ? true : false ;
+
+}
+  
+booWho(null) ;
