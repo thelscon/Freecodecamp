bc390cff29aedfb61f2c18e28a355e527b87aeb3
thelscon <thelscon@gmail.com>
2022-02-10 23:31:16 +0200

    next

--- Spinal_Tap_Case.js
+++ Spinal_Tap_Case.js
@@ -1,7 +1,21 @@
 /*             Случай спинномозговой пункции
 Преобразование строки в спинной регистр. В спинном регистре все строчные слова соединены тире. */
 
-//первый способ, без RegExp
+//второй способ, с использованием RegExp
+function spinalCase(str) {
+    const   pattern0 = /[a-z][A-Z]/g ,      // aA => a-a
+            pattern1 = /\s|_/g ,            // ' ' | '_' => '-'
+            pattern2 = /[A-Z]|(-[A-Z])/g ;  // A | -A => a | -a
+    str = str   .replace ( pattern0 , value => `${value[0]}-${value[1].toLowerCase()}` )    // aA => a-a
+                .replace ( pattern1 , '-' )                                                 // ' ' | '_' => '-'
+                .replace ( pattern2 , value => value.toLowerCase() ) ;                      // A | -A => a | -a
+
+    return str ;
+}
+  
+spinalCase('This Is Spinal Tap');
+
+/* //первый способ, без RegExp
 function spinalCase(str) {
 
     while ( str.includes ( ' ' ) ) {
@@ -29,4 +43,4 @@ function spinalCase(str) {
     return str;
 }
   
-console.log ( spinalCase('This Is Spinal Tap') );
+spinalCase('This Is Spinal Tap'); */
