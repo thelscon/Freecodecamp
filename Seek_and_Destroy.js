ff03d7e266e533fa6c619de9ebf8d24a204b496a
thelscon <thelscon@gmail.com>
2022-02-10 19:17:52 +0200

    next

--- Seek_and_Destroy.js
+++ Seek_and_Destroy.js
@@ -1,8 +1,20 @@
 /*             Найти и уничтожить
-Вам будет предоставлен начальный массив (первый аргумент в destroyerфункции), за которым следуют один или несколько 
+Вам будет предоставлен начальный массив (первый аргумент в destroyer функции), за которым следуют один или несколько 
 аргументов. Удалите все элементы из исходного массива, которые имеют то же значение, что и эти аргументы.
 Примечание. Вы должны использовать arguments объект. */
 
+//второй способ, удаление методом filter одинаковых значений
+function destroyer(arr) {
+    arr = arguments[0] ;
+    for ( let key = 1, tmpArray = arguments[key] ; key <= arguments.length ; ++key ) {
+        arr = arr.filter ( value => value !== arguments[key] ) ;
+    }
+    return arr;
+}
+  
+destroyer([1, 2, 3, 1, 2, 3], 1, 2) ;
+
+/* //первый способ, удаление методом splice
 function destroyer(arr) {
     const fullArray = arguments[0] ;
     for ( let key = 1 ; key <= arguments.length ; ++key ) {
@@ -20,4 +32,4 @@ function destroyer(arr) {
     return fullArray;
   }
   
-  console.log ( destroyer([1, 2, 3, 1, 2, 3], 1, 2) ) ;
+ destroyer([1, 2, 3, 1, 2, 3], 1, 2) ; */
